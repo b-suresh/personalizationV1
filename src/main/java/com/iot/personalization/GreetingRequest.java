4c4f45b40cbbbcecd03fa699887dc56127cf40f4
Suresh Balasubramanian <b_suresh@rocketmail.com>
2016-03-27 12:40:24 +0530

    Added JSON support to consume JSON input

---
+++ src/main/java/com/iot/personalization/GreetingRequest.java
@@ -0,0 +1,32 @@
+package com.iot.personalization;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class GreetingRequest {
+	private String messageId;
+	private String name;
+	private String rfid;
+	
+	public GreetingRequest(){
+	}
+	
+	public GreetingRequest( String messageId, String name, String rfid ){
+		this.messageId = messageId;
+		this.name = name;
+		this.rfid = rfid;
+	}
+	
+	@JsonProperty
+	public String getMessageId(){
+		return messageId;
+	}
+	@JsonProperty
+	public String getName() {
+		return name;
+	}
+	@JsonProperty
+	public String getRfid(){
+		return rfid;
+	}
+
+}
